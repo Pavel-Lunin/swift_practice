30af4eab82d425728ae5fa3ea5a5677dd713df71
Pavel <pavelunin@icloud.com>
2024-03-17 21:03:11 +0300

    add button and label

--- OutletAndAction/OutletAndAction/ViewController.swift
+++ OutletAndAction/OutletAndAction/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func ChangeTextInLabel(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
 }
 
