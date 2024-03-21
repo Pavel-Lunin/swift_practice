a7bc0da6b70a9cef80a37780954897cdbbae0565
Pavel <pavelunin@icloud.com>
2024-03-21 09:36:40 +0300

    added style

--- WeekdayFinder/WeekdayFinder/ViewController.swift
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -8,12 +8,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // TF -TextField
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay() {
+        
     }
-
 
 }
 
