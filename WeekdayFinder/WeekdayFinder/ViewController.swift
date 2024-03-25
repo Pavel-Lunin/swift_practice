b23df84020f5cca8cd1932dee6f17b4f0520fa68
Pavel <pavelunin@icloud.com>
2024-03-25 09:41:30 +0300

    added calendar processing

--- WeekdayFinder/WeekdayFinder/ViewController.swift
+++ WeekdayFinder/WeekdayFinder/ViewController.swift
@@ -15,8 +15,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
-    @IBAction func findDay() {
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+            return .lightContent
+        }
+    
+    @IBAction func findWeekDay() {
+        let calendar = Calendar.current
         
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(dateTF.text!)
+        dateComponents.month = Int(monthTF.text!)
+        dateComponents.year = Int(yearTF.text!)
+        
+        let date = calendar.date(from: dateComponents)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let weekDay = dateFormatter.string(from: date!)
+        
+        resultLabel.text = weekDay
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 
 }
