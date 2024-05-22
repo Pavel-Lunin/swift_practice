19f87a7a9f9e4c9fdf554caabeabfd523684abd2
Pavel <pavelunin@icloud.com>
2024-05-22 10:03:12 +0300

    Added audio player logics

---
+++ SliderSwiftUI/SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Pavel on 21.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
